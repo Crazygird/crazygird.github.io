da74bed8fd6438bd674c3860246682e888b475b6
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-13 14:00:03 -0800

    ok

--- html/message/script.js
+++ html/message/script.js
@@ -2,10 +2,11 @@
 var messages = document.getElementById("messages");
 var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
+var username = document.getElementById("username")
 
 button.addEventListener("click", function(){
      var newMessage = document.createElement("li");
-     newMessage.innerHTML = textbox.value;
+     newMessage.innerHTML = username.value.concat(" ", textbox.value)
      messages.appendChild(newMessage);
      textbox.value = "";
 });
