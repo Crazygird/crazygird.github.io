156725997b5bcbb8f643281dde11cf6a020e8b4c
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-14 11:28:59 -0800

    oh my goododood

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function newFact() {
     var facts = ['RIP Internet Explorer', 'Potato', '<a href="https://bsd.instructure.com/courses/114205/pages/weeks-21-22-feb-6-feb-17?module_item_id=3348875">Will this work?</a>'];
     var randomFact = Math.floor(Math.random() * facts.length);
-    document.getElementById('factDisplay').innerHTML = facts[randomFact];
+    document.getElementById('myTxt').innerHTML = facts[randomFact];
   })();
