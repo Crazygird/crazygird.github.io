6f73f6fbc79dec0a3d625815b8ff3324e986d51c
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-27 13:41:52 -0800

    cleaned up

--- index.js
+++
@@ -1,5 +0,0 @@
-(function newFact() {
-    var facts = ['RIP Internet Explorer', 'Potato', '<a href="https://bsd.instructure.com/courses/114205/pages/weeks-21-22-feb-6-feb-17?module_item_id=3348875">Will this work?</a>'];
-    var randomFact = Math.floor(Math.random() * facts.length);
-    document.getElementById('myTxt').innerHTML = facts[randomFact];
-  })();
