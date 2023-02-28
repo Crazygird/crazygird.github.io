2bfa738a0bf99d4e00634a114705c767e5641e77
Mide <95974074+Crazygird@users.noreply.github.com>
2023-02-28 11:16:46 -0800

    phta

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$messages = array(
+    "Welcome to my website!",
+    "Thanks for visiting!",
+    "Have a nice day!",
+    "Come back soon!"
+);
+
+$message = $messages[array_rand($messages)];
+?>
