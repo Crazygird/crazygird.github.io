c766ca0e3444a96cb986bf452fe18395fc12244c
Mide <95974074+Crazygird@users.noreply.github.com>
2023-02-28 15:20:02 -0800

    i fix codql error

--- html/message/script.js
+++ html/message/script.js
@@ -1,31 +1,25 @@
+const messages = document.getElementById("messages");
+const textbox = document.getElementById("textbox");
+const button = document.getElementById("button");
+const username = document.getElementById("username");
 
-var messages = document.getElementById("messages");
-var textbox = document.getElementById("textbox");
-var button = document.getElementById("button");
-var username = document.getElementById("username")
+button.addEventListener("click", function() {
+  const newMessage = document.createElement("li");
+  const sanitizedUsername = DOMPurify.sanitize(username.value);
+  const sanitizedTextbox = DOMPurify.sanitize(textbox.value);
+  newMessage.innerHTML = sanitizedUsername.concat(": ", sanitizedTextbox);
+  messages.appendChild(newMessage);
+  textbox.value = "";
 
-button.addEventListener("click", function(){
-     var newMessage = document.createElement("li");
-     newMessage.innerHTML = username.value.concat(": ",textbox.value);
-     messages.appendChild(newMessage);
-     textbox.value = "";
-     const fs = require('fs');
-
-// Load existing data from the file
-let data = fs.readFileSync('data.json');
-
-// Parse the JSON data
-let jsonData = JSON.parse(data);
-
-// Add new data to the JSON object
-jsonData.newData = newMessage.innerHTML;
-
-// Convert the updated JSON object to a string
-let updatedData = JSON.stringify(jsonData);
-
-// Write the updated data to the file
-fs.writeFileSync('data.json', updatedData);
-
+  const fs = require("fs");
+  // Load existing data from the file
+  const data = fs.readFileSync("data.json");
+  // Parse the JSON data
+  const jsonData = JSON.parse(data);
+  // Add new data to the JSON object
+  jsonData.newData = newMessage.innerHTML;
+  // Convert the updated JSON object to a string
+  const updatedData = JSON.stringify(jsonData);
+  // Write the updated data to the file
+  fs.writeFileSync("data.json", updatedData);
 });
-
-
